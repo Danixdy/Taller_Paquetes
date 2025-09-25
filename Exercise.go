5e06b5b4504b483c53413b7773177f3771bdd1e3
Danixdy <dfpazminio@gmail.com>
2025-09-24 23:08:25 -0500

    Subir paquete resolucion

---
+++ Exercise.go
@@ -0,0 +1,23 @@
+package main
+
+import "fmt"
+
+
+func main()  {
+	var numero int
+	
+	fmt.Println("Ingrese un numero:")
+	Scan(&numero)
+
+	for i:=1; i<=numero;i+=1{
+		var  numPer int =0
+		
+		for j:=1; j<i; j++{
+			suma:= 0
+			if {
+				
+			}
+		}
+		fmt.Println(numPer)
+	}
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/Danixdy/Taller_Paquetes
+
+go 1.25.1
